de28962305a3e8d72f2968c62234ccae673c439b
Petro Tafilaj <itspedro82@gmail.com>
2022-02-20 13:46:29 +0100

    internal/repository

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -7,10 +7,13 @@ import (
 	"net/http"
 
 	"github.com/petrotafilaj/bookings/internal/config"
+	"github.com/petrotafilaj/bookings/internal/driver"
 	"github.com/petrotafilaj/bookings/internal/forms"
 	"github.com/petrotafilaj/bookings/internal/helpers"
 	"github.com/petrotafilaj/bookings/internal/models"
 	"github.com/petrotafilaj/bookings/internal/render"
+	"github.com/petrotafilaj/bookings/internal/repository"
+	"github.com/petrotafilaj/bookings/internal/repository/dbrepo"
 )
 
 // Repo the repository used by the handlers
@@ -19,12 +22,14 @@ var Repo *Repository
 // Repository is the repository type
 type Repository struct {
 	App *config.AppConfig
+	DB  repository.DatabaseRepo
 }
 
 // NewRepo creates a new repository
-func NewRepo(a *config.AppConfig) *Repository {
+func NewRepo(a *config.AppConfig, db *driver.DB) *Repository {
 	return &Repository{
 		App: a,
+		DB:  dbrepo.NewPostgresRepo(db.SQL, a),
 	}
 }
 
