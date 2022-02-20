c7d33ec9a47c55297759ef11a5678e198d4b08cc
Petro Tafilaj <itspedro82@gmail.com>
2022-02-20 20:59:26 +0100

    Searching for availability by room

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,9 +1,15 @@
 package repository
 
-import "github.com/petrotafilaj/bookings/internal/models"
+import (
+	"time"
+
+	"github.com/petrotafilaj/bookings/internal/models"
+)
 
 type DatabaseRepo interface {
 	AllUsers() bool
 
-	InsertReservation(res models.Reservation) error
+	InsertReservation(res models.Reservation) (int, error)
+	InsertRoomRestriction(r models.RoomRestriction) error
+	SearchAvailabilityByDates(start, end time.Time, roomID int) (bool, error)
 }
