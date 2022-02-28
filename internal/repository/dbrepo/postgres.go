a9ceab3f6bcdc8c1ee2e7a1e2f4f3ea396d1a8d1
Petro Tafilaj <itspedro82@gmail.com>
2022-02-28 15:24:27 +0100

    cmd/web/config

--- internal/repository/dbrepo/postgres.go
+++ internal/repository/dbrepo/postgres.go
@@ -391,7 +391,8 @@ func (m *postgresDBRepo) UpdateReservation(u models.Reservation) error {
 	defer cancel()
 
 	query := `
-	update reservations set first_name = $1, last_name = $2, email = $3, phone = $4, updated_at = $5	
+	update reservations set first_name = $1, last_name = $2, email = $3, phone = $4, updated_at = $5
+	where id = $6
 `
 
 	_, err := m.DB.ExecContext(ctx, query,
@@ -400,6 +401,7 @@ func (m *postgresDBRepo) UpdateReservation(u models.Reservation) error {
 		u.Email,
 		u.Phone,
 		time.Now(),
+		u.ID,
 	)
 
 	if err != nil {
